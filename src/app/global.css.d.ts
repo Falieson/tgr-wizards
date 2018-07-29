31ef05460a43a850f6a681b7c391fadafa2c4c98
Florian Mettetal <florian.mettetal@gmail.com>
2018-07-29 07:02:16 -0700

    :construction: Wizard SimpleComponent outline

--- src/app/global.css.d.ts
+++ src/app/global.css.d.ts
@@ -2,4 +2,4 @@ export interface IGlobalCss {
 
 }
 
-export const locals: IGlobalCss
+export const locals: IGlobalCss;
--- src/modules/wizard/simple/wizard.simple.scss.d.ts
+++ src/modules/wizard/simple/wizard.simple.scss.d.ts
@@ -1,14 +1,14 @@
 export interface IWizardSimpleScss {
-  'fill_primary': string
-  'fill_secondary': string
-  'bg_primary': string
-  'bg_secondary': string
-  'accent_primary': string
-  'accent_secondary': string
-  'accent_primary--dark': string
-  'accent_secondary--dark': string
-  'container': string
-  'border': string
+  'fill_primary': string;
+  'fill_secondary': string;
+  'bg_primary': string;
+  'bg_secondary': string;
+  'accent_primary': string;
+  'accent_secondary': string;
+  'accent_primary--dark': string;
+  'accent_secondary--dark': string;
+  'container': string;
+  'border': string;
 }
 
-export const locals: IWizardSimpleScss
+export const locals: IWizardSimpleScss;
